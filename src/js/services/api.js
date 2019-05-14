0be53a176ef5a227535a96e70d03f08cb63f9c8a
Andrii Dederia <derya_86@ukr.net>
2019-05-14 16:15:29 +0300

    server created

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
-const API_KEY = '10456559-ece128cde22be6e54d79b9b25';
+import axios from "axios";
+const API_KEY = "10456559-ece128cde22be6e54d79b9b25";
 
 export const getImages = ({ query, page }) => {
-  const url = `https://pixabay.com/api/?image_type=photo&q=${query}&per_page=12&page=${page}&key=${API_KEY}`;
-
-  return axios
-    .get(url)
-    .then(res => res.data.hits)
-    .catch(err => console.log('axios err : ', err));
+  const url = `https://findpicserver.herokuapp.com/`;
+  // const url = `http://localhost:8080/`;
+  return axios({
+    method: "get",
+    url: url,
+    data: {
+      query: query,
+      page: page
+    },
+    headers: { Xquery: query, Xpage: page }
+  })
+    .then(res => res.data.data.hits)
+    .catch(err => console.log("axios err : ", err));
 };
